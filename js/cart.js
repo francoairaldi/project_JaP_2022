2a092e0a70eaeba97b358331c219bbfef59183b6
francoairaldi <f.airaldi@hotmail.com>
2022-10-20 07:47:17 -0300

    entrega 5 - carrito

---
+++ js/cart.js
@@ -0,0 +1,61 @@
+const cart = CART_INFO_URL + "25801.json";
+cartInfo = {};
+
+function showCart(array){
+    //inicio estructura del carrito
+    htmlContentToAppend = `
+    <div>
+        <table class="table cart-table">
+            <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Costo</th>
+                    <th scope="col">Cantidad</th>
+                    <th scope="col">Subtotal</th>
+                    <th class="tablefixer"></th>
+                </tr>
+            </thead>
+            <tbody>`;
+    //productos del carrito
+    for (i=0; i<array.length; i++){
+        let product = array[i];
+        htmlContentToAppend += `
+                <tr>
+                    <td class="tableImg"><img class="cart-img" src="${product.image}"></td>
+                    <td>${product.name}</td>
+                    <td>${product.currency} ${product.unitCost}</td>
+                    <td class="cartInput"><input type="number" id="inputCount${product.id}" name="count" value="${product.count}" min="0"></td>
+                    <td id="subtotal${product.id}">${product.currency} ${product.unitCost*product.count}</td>
+                    <td class="tablefixer"></td>
+                </tr>`
+    }
+    //final estructura del carrito
+    htmlContentToAppend += `
+            </tbody>
+        </table>
+    </div>
+    `;
+    document.getElementById("cartBody").innerHTML = htmlContentToAppend;
+    
+    //addEventListeners para el calculo del subtotal
+    for(i=0; i<array.length; i++){
+        let product = array[i];
+        document.getElementById(`inputCount${product.id}`).addEventListener("input", function(event){
+            document.getElementById(`subtotal${product.id}`).innerHTML = `${product.currency} ${product.unitCost*document.getElementById(`inputCount${product.id}`).value}`
+    })
+    }
+}
+
+
+//ejecucion del codigo
+document.addEventListener("DOMContentLoaded", function(event){
+
+    getJSONData(cart).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            cartInfo = resultObj.data.articles;
+            showCart(cartInfo)
+        }
+    })
+
+})
